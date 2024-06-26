597aea7cf5b960c53f8ed18e44e0eaf501f7a6e8
Ybz1234 <122832333+Ybz1234@users.noreply.github.com>
2024-06-26 22:54:44 +0300

    Initial commit

---
+++ BinaryNode.cs
@@ -0,0 +1,58 @@
+﻿namespace BinaryTree
+{
+    internal class BinaryNode<T>
+    {
+        public T Value { get; set; }
+        public BinaryNode<T> Left { get; set; }
+        public BinaryNode<T> Right { get; set; }
+        public BinaryNode(T value)
+        {
+            this.Value = value;
+            this.Left = this.Right = null;
+        }
+        public BinaryNode(BinaryNode<T> left, T value, BinaryNode<T> right)
+        {
+            this.Left = left;
+            this.Value = value;
+            this.Right = right;
+        }
+        public T GetValue()
+        {
+            return Value;
+        }
+        public BinaryNode<T> GetLeft()
+        {
+            return Left;
+        }
+        public BinaryNode<T> GetRight()
+        {
+            return Right;
+        }
+
+        public bool HasLeft()
+        {
+            return Left != null;
+        }
+
+        public bool HasRight()
+        {
+            return Right != null;
+        }
+        public void SetValue(T value)
+        {
+            this.Value = value;
+        }
+        public void SetLeft(BinaryNode<T> left)
+        {
+            Left = left;
+        }
+        public void SetRight(BinaryNode<T> right)
+        {
+            Right = right;
+        }
+        public override string ToString()
+        {
+            return Left + " " + Value + " " + Right;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            BinaryNode<int> binaryNode = new BinaryNode<int>(1);
+            BinaryNode<int> binaryNode2 = new BinaryNode<int>(2);
+            BinaryNode<int> binaryNode3 = new BinaryNode<int>(3);
+            BinaryNode<int> binaryNode4 = new BinaryNode<int>(5);
+            BinaryNode<int> binaryNode5 = new BinaryNode<int>(6);
+            
+            binaryNode.SetLeft(binaryNode2);
+            binaryNode.SetRight(binaryNode3);
+            binaryNode2.SetLeft(binaryNode4);
+            binaryNode2.SetRight(binaryNode5);
+
+            Console.Write("Pre order: ");
+            PreOrder(binaryNode);
+            Console.Write("\nIn order: ");
+            InOrder(binaryNode);
+            Console.Write("\nPost order: ");
+            PostOrder(binaryNode);
+            Console.Write("\nPrint even: ");
+            PrintEven(binaryNode);
+            Console.WriteLine("\nCounter: " + NumOfNodes(binaryNode));
+            Console.WriteLine($"Is exist: {IsExists(binaryNode, 7)}");
+            Console.WriteLine($"Does each father has 2 children: {IsEachFatherHas2Child(binaryNode)}");
+            Console.WriteLine($"Height: {Height(binaryNode)}");
+        }
+        static void PreOrder<T>(BinaryNode<T> t)
+        {
+            if (t == null) return;
+            Console.Write($"{t.GetValue()} ➡  ");
+            PreOrder(t.GetLeft());
+            PreOrder(t.GetRight());
+        }
+        static void InOrder<T>(BinaryNode<T> t)
+        {
+            if (t == null) return;
+            InOrder(t.GetLeft());
+            Console.Write($"{t.GetValue()} ➡  ");
+            InOrder(t.GetRight());
+        }
+        static void PostOrder<T>(BinaryNode<T> t)
+        {
+            if (t == null) return;
+            PostOrder(t.GetLeft());
+            PostOrder(t.GetRight());
+            Console.Write($"{t.GetValue()} ➡  ");
+        }
+        static void PrintEven(BinaryNode<int> t)
+        {
+            if (t == null) return;
+            if (t.GetValue() % 2 == 0)
+                Console.Write($"{t.GetValue()} ➡  ");
+            PrintEven(t.GetLeft());
+            PrintEven(t.GetRight());
+        }
+        static int NumOfNodes<T>(BinaryNode<T> t)
+        {
+            if (t == null) return 0;
+            return 1 + NumOfNodes(t.GetLeft()) + NumOfNodes(t.GetRight());
+        }
+        static bool IsExists<T>(BinaryNode<T> t, T value)
+        {
+            if (t == null) return false;
+            if (value.Equals(t.GetValue())) return true;
+            return IsExists(t.GetLeft(), value) || IsExists(t.GetRight(), value);
+        }
+        static bool IsEachFatherHas2Child<T>(BinaryNode<T> t)
+        {
+            if (t == null)
+                return true;
+            if (t.HasLeft() && !t.HasRight())
+                return false;
+            if (!t.HasLeft() && t.HasRight())
+                return false;
+            if(!t.HasLeft() && t.HasRight())
+                return true;
+            return IsEachFatherHas2Child(t.GetLeft()) && IsEachFatherHas2Child(t.GetRight());
+        }
+        static int Height<T>(BinaryNode<T> t)
+        {
+            if (t == null) return 0;
+            return 1 + Math.Max(Height(t.GetLeft()), Height(t.GetRight()));
+        }
+    }
+}
